339ee38b93167b49b4b46508d3ba0be9ecee436f
Savidya448 <as20240448@sci.sjp.ac.lk>
2025-10-25 13:03:14 +0530

    define structures

--- main.c
+++ main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
-}
+#define MAX_CITIES 30
+#define MAX_DELIVERIES 50
+#define VEHICLE_COUNT 3
+#define MAX_NAME_LENGTH 50
+#define FUEL_PRICE 310.0
+
+typedef struct {
+    char name[MAX_NAME_LENGTH];
+} City;
+
+typedef struct {
+    char name[10];
+    int capacity;
+    float ratePerKm;
+    float speed;
+    float efficiency;
+} Vehicle;
+
+typedef struct {
+    int src, dest;
+    float weight;
+    int vehicleType;
+    float distance;
+    float baseCost, fuelUsed, fuelCost, totalCost, profit, customerCharge, time;
+} Delivery;
